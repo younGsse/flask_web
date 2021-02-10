e54a717f87ce9e5501a3a783ec57eeade2e23e98
young <wjdtp93@gmail.com>
2021-02-10 11:00:47 +0900

    start flask

---
+++ views/main_views.py
@@ -0,0 +1,16 @@
+from flask import Blueprint
+
+bp = Blueprint('main', __name__, url_prefix='/')
+
+# @bp.route('/')
+# def hello_pybo():
+#     return 'Hello, Pybo'
+
+
+@bp.route('/hello')
+def hello_pybo():
+    return 'Hello, Pybo!'
+
+@bp.route('/')
+def index():
+    return 'Pybo index'
